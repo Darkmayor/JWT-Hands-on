a5d01fe61cb8834df1c033de770f33a5720ec891
Darkmayor <sanketkalokhe123@gmail.com>
2024-01-07 10:57:17 +0530

    Initializing the project

---
+++ src/test/java/com/Sanket/JWT_Auth_Demo/JwtAuthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sanket.JWT_Auth_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
